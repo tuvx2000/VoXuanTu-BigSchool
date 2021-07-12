36f5f3edeaec591e1dedde8de687e162ec033385
XuanTu <tuvx2000@gmail.com>
2021-07-12 09:57:31 +0700

    TuVX - finished

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VoXuanTu_BigSchool001")]
+[assembly: AssemblyTitle("LeHuuTuong_BigSchool001")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VoXuanTu_BigSchool001")]
+[assembly: AssemblyProduct("LeHuuTuong_BigSchool001")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
